4ab3e62871b9babf7373793603a36d2dfd37d1b9
GOPALAKRISHNAREDDY <gopalakrishna.vintha@xcubelabs.com>
2021-08-16 17:12:16 +0530

    PlayerMovement done

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float moveSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * Time.deltaTime * moveSpeed*horizontal);
+        transform.Translate(Vector3.up * Time.deltaTime * moveSpeed*vertical);
+
+
+        //Player Y Boundarys
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -4.1f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.1f, 0);
+        }
+        //Player X Boundarys
+        if (transform.position.x >=10f )
+        {
+            transform.position = new Vector3(-10f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -10f)
+        {
+            transform.position = new Vector3(10f,transform.position.y, 0);
+        }
     }
 }
